6265c54de2fe41f96904593ee80f1b1e5eadd24b
Unknown <derik@oagro.com.br>
2019-05-04 16:14:02 -0300

    Creating show for restaurant and your products

--- src/services/api.js
+++ src/services/api.js
@@ -3,13 +3,14 @@ import axios from "axios";
 const url = axios.create({ baseURL: "http://localhost:3001" })
 
 export default {
-  // filtrando por categorias
   loadRestaurants: (category = null) => {
     let filter = category ? `?category=${category.title}` : ""
     return url.get(`/restaurants${filter}`)
   },
 
   searchRestaurants: (search) => url.get(`/restaurants/search?q=${search}`),
+
+  getRestaurant: (id) => url.get(`restaurants/${id}`),
 
   loadCategories: () => url.get("/categories")
 }
