1126247719af1d8758ef86a9ea5fd993838a2eb6
Unknown <derik@oagro.com.br>
2019-05-04 15:44:23 -0300

    Adding filter of categories

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,10 @@ import axios from "axios";
 const url = axios.create({ baseURL: "http://localhost:3001" })
 
 export default {
-  loadRestaurants: () => url.get("/restaurants")
+  // filtrando por categoria
+  loadRestaurants: (category = null) => {
+    let filter = category ? `?category=${category.title}` : ""
+    return url.get(`/restaurants${filter}`)
+  },
+  loadCategories: () => url.get("/categories")
 }
